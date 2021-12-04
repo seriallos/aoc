681b2daa91035f6ccff3bdb3160192d17b8f75af
Dave Hendler <sollaires@gmail.com>
2021-12-03 20:55:42 -0800

    Add placeholders for answer variables and print them

--- template/answer.js
+++ template/answer.js
@@ -5,6 +5,20 @@ import _ from 'lodash';
 // eslint-disable-next-line no-unused-vars
 function solve(input) {
   // const inputInts = _.map(input, i => parseInt(i, 10));
+
+  // part 1
+  let part1Answer = null;
+
+
+  console.log(`Part 1: ${part1Answer}`);
+
+
+
+  // part 2
+  let part2Answer = null;
+
+
+  console.log(`Part 2: ${part2Answer}`);
 }
 
 export default solve;
