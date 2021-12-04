67ed680d75b42932b5b32c9e91ebd63c6696cc13
Dave Hendler <sollaires@gmail.com>
2021-12-03 20:51:25 -0800

    Do not remove empty lines, they are important for some problems

--- util.js
+++ util.js
@@ -10,7 +10,7 @@ export const getInput = async (year, day, filename) => {
     const __dirname = path.dirname(__filename);
     const inputPath = path.join(__dirname, `${year}`, `day${day}`, filename);
     const rawData = await fs.readFile(inputPath, 'utf8');
-    const input = _.filter(_.split(rawData, '\n'));
+    const input = _.split(rawData, '\n');
     return input;
   } catch (err) {
     const e = new Error(`Year ${year} Day ${day} ${filename} cannot be loaded`);
