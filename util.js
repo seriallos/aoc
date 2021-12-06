7dbccb164718a3fa53dae4632ed19928c56b5baa
Dave Hendler <sollaires@gmail.com>
2021-12-05 16:29:55 -0800

    Add initial drawGrid helper

--- util.js
+++ util.js
@@ -1,8 +1,10 @@
+/* eslint-env node */
 import { promises as fs } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 import _ from 'lodash';
+import chalk from 'chalk';
 
 export const getInput = async (year, day, filename) => {
   try {
@@ -17,4 +19,51 @@ export const getInput = async (year, day, filename) => {
     e.cause = err;
     throw e;
   }
+};
+
+export const drawGrid = (data, options) => {
+  const opts = _.defaults(
+    options,
+    {
+      empty: chalk.gray('.'),
+      xMin: 0,
+      xMax: null,
+      yMin: 0,
+      yMax: null,
+      hashes: true,
+    },
+  );
+
+  // find max bounds
+  for (let y = 0; y < data.length; y += 1) {
+    opts.yMax = y;
+    if (data[y] && data[y].length > opts.xMax) {
+      opts.xMax = data[y].length;
+    }
+  }
+
+  if (opts.xMax > 80) {
+    console.log(chalk.red('!! drawGrid: Grid is too wide to show'));
+    return;
+  }
+
+  if (opts.hashes) {
+    process.stdout.write('    ');
+    for (let x = opts.xMin; x <= opts.xMax; x += 1) {
+      process.stdout.write(chalk.gray(`${x % 10} `));
+    }
+    process.stdout.write('\n');
+    process.stdout.write('\n');
+  }
+  for (let y = opts.yMin; y <= opts.yMax; y += 1) {
+    if (opts.hashes) {
+      process.stdout.write(chalk.gray(`${y % 10}   `));
+    }
+    for (let x = opts.xMin; x <= opts.xMax; x += 1) {
+      process.stdout.write((data[y] && data[y][x]) ? String(data[y][x]) : opts.empty);
+      process.stdout.write(' ');
+    }
+    process.stdout.write('\n');
+  }
+  process.stdout.write('\n');
 };
