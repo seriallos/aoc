81d9a03026f45adfca1bef40db33565319b415d6
Dave Hendler <sollaires@gmail.com>
2021-12-06 21:34:31 -0800

    Add warning for when logging is disabled

--- template/answer.js
+++ template/answer.js
@@ -5,11 +5,16 @@ import _ from 'lodash';
 const LOG_TEST = true;
 const LOG_REAL = false;
 
+let noLogWarned = false;
+
 export const part1 = (input, isTest) => {
   // eslint-disable-next-line no-unused-vars
   const log = (...args) => {
     if ((LOG_TEST && isTest) || (LOG_REAL && !isTest)) {
       console.log(...args);
+    } else if (!noLogWarned) {
+      console.log('Logging is disabled for this input');
+      noLogWarned = true;
     }
   };
 
@@ -23,6 +28,9 @@ export const part2 = (input, isTest) => {
   const log = (...args) => {
     if ((LOG_TEST && isTest) || (LOG_REAL && !isTest)) {
       console.log(...args);
+    } else if (!noLogWarned) {
+      console.log('Logging is disabled for this input');
+      noLogWarned = true;
     }
   };
 
