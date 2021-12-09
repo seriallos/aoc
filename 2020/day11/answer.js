f4406279d990fb585499c729b65499e1b520c829
Dave Hendler <sollaires@gmail.com>
2021-12-08 20:40:47 -0800

    More WIP: 2020 day 11

--- 2020/day11/answer.js
+++ 2020/day11/answer.js
@@ -18,23 +18,64 @@ export const part1 = (input, isTest) => {
   let answer = null;
 
   const layout = input;
-  let seats = _.map(layout, row => _.map(row, () => '.'));
+  let seats = _.map(layout, row => _.map(row, () => false));
+  let prevSeats;
   drawGrid(layout);
-  drawGrid(seats);
 
   const updateSeats = () => {
-    const newSeats = [...seats];
+    const newSeats = _.map(seats, row => [...row]);
     for (let y = 0; y < seats.length; y += 1) {
       const row = seats[y];
       for (let x = 0; x < row.length; x += 1) {
-        const seat = seats[y][x];
-        if (seats[y][x] === '.') {
-        } else {
-          // i am here
+        if (layout[y][x] === 'L') {
+          let neighbors = 0;
+
+          for (let y1 = y - 1; y1 <= y + 1; y1 += 1) {
+            for (let x1 = x - 1; x1 <= x + 1; x1 += 1) {
+              if (x1 >= 0 && y1 >= 0 && !(x1 === x && y1 === y)) {
+                if (seats[y1] && seats[y1][x1]) {
+                  neighbors += 1;
+                }
+              }
+            }
+          }
+
+          if (!seats[y][x] && neighbors === 0) {
+            newSeats[y][x] = true;
+          } else if (seats[y][x] && neighbors >= 4) {
+            newSeats[y][x] = false;
+          }
         }
       }
     }
+    prevSeats = seats;
+    seats = newSeats;
   };
+
+  let iterations = 0;
+  let changed = true;
+  while (changed) {
+    answer = 0;
+    changed = false;
+    iterations += 1;
+    updateSeats();
+    drawGrid(seats);
+
+    for (let y = 0; y < seats.length; y += 1) {
+      const row = seats[y];
+      for (let x = 0; x < row.length; x += 1) {
+        if (seats[y][x] !== prevSeats[y][x]) {
+          changed = true;
+        }
+        if (seats[y][x]) {
+          answer += 1;
+        }
+      }
+    }
+  }
+
+  log('iterations', iterations);
+
 
   return answer;
 }
